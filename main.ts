7e432b9a24cfa1fdc5cb70b96948ed1b4863261c
HenryKhosasih <68894872+HenryKhosasih@users.noreply.github.com>
2024-12-15 08:10:58 +1100

    use local provider for creating readme file

--- main.ts
+++ main.ts
@@ -1,13 +1,24 @@
 import { Construct } from "constructs";
-import { App, TerraformOutput, TerraformStack } from "cdktf";
+import { App, TerraformStack } from "cdktf";
+import { file, provider } from "@cdktf/provider-local";
+import * as path from "path";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
-    new TerraformOutput(this, "helloWorld", {
-      value: "Hello World",
+    new provider.LocalProvider(this, "local", {});
+
+    const projectDirectory = path.join(
+      process.env.INIT_CWD!,
+      "./authors-project"
+    );
+    const projectName = "project-1";
+    const basePath = `${projectDirectory}/${projectName}`;
+
+    new file.File(this, "readme-file", {
+      filename: `${basePath}/README.md`,
+      content: `# ${projectName}\n\nThis is the ${projectName} project`,
     });
   }
 }
